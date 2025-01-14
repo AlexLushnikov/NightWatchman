53df2113d9d8075b5f20671d254e01c563a5b18d
Kirill Islibaev <devois1337@gmail.com>
2025-01-14 18:27:48 +0400

    Set up objects on the scene

--- Assets/NightWatchman/Scripts/LevelManagment/EInteractableIds.cs
+++ Assets/NightWatchman/Scripts/LevelManagment/EInteractableIds.cs
@@ -5,6 +5,19 @@
         Desk,
         Chair_3,
         Table,
-        CoatHook
+        CoatHook,
+        Firewood,
+        Chair,
+        Chair_1,
+        Chair_2,
+        Barrel_1,
+        Potion_1,
+        Carpet_Red,
+        Bench,
+        Bucket,
+        Candlier,
+        Fireplace,
+        Carpet_Green,
+        Shelf
     }
 }
