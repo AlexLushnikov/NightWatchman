6745abe75b5b97616497443c95af0513c9e4845f
Kirill Islibaev <devois1337@gmail.com>
2025-01-16 17:40:44 +0400

    Make second location

--- Assets/NightWatchman/Scripts/LevelManagment/EInteractableIds.cs
+++ Assets/NightWatchman/Scripts/LevelManagment/EInteractableIds.cs
@@ -1,23 +1,36 @@
-﻿namespace NightWatchman
+namespace NightWatchman
 {
     public enum EInteractableIds
     {
         Desk,
-        Chair_3,
         Table,
+        Table_1,
         CoatHook,
         Firewood,
         Chair,
         Chair_1,
         Chair_2,
+        Chair_3,
         Barrel_1,
-        Potion_1,
         Carpet_Red,
+        Carpet_Red_1,
         Bench,
         Bucket,
         Candlier,
         Fireplace,
+        Fireplace_1,
         Carpet_Green,
-        Shelf
+        Shelf,
+        WeaponRack_Small,
+        Chest,
+        Chest_1,
+        Big_Shelf,
+        Big_Shelf_1,
+        Big_Shelf_2,
+        Banner,
+        Table_Big,
+        Throne,
+        Throne_1,
+        Firewood_1
     }
 }
